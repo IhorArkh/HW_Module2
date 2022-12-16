347a3459faaae8d2e1d8986306ca260adc93710b
Ihor Arkhypenko <wasbornindecember@gmail.com>
2022-12-16 23:12:16 +0200

    HW2 online-store ready

---
+++ HW_Module2/HW_2_2_Online_Store/Item.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW_2_2_Online_Store
+{
+    public class Item
+    {
+        public string NameOfItem { get; set; }
+        public double PriceOfItem { get; set; }
+        public Item(string nameofitem, double priceofitem)
+        {
+            NameOfItem = nameofitem;
+            PriceOfItem = priceofitem;
+        }   
+    }
+}
---
+++ HW_Module2/HW_2_2_Online_Store/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Security.Cryptography.X509Certificates;
+
+namespace HW_2_2_Online_Store
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Store store = new Store();
+            store.ShowBasket(store.AddItemsToBasket(store.PrintListOfItems()));
+        }
+    }
+}
