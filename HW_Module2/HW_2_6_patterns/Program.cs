1efd819525132b1cfc77732d7661e10eb92cc948
Ihor Arkhypenko <wasbornindecember@gmail.com>
2022-12-24 17:25:17 +0200

    Prototype added

--- HW_Module2/HW_2_6_patterns/Program.cs
+++ HW_Module2/HW_2_6_patterns/Program.cs
@@ -1,15 +1,16 @@
 ﻿namespace HW_2_6_patterns
 {
-    class Car
-    {
-
-    }
-    class CarBuilder
-    {
-        private Car car; 
-    }
     internal class Program
     {
+        //Prototype
+        public static void DisplayValues(Person p)
+        {
+            Console.WriteLine("      Name: {0:s}, Age: {1:d}, BirthDate: {2:MM/dd/yy}",
+                p.Name, p.Age, p.BirthDate);
+            Console.WriteLine("      ID#: {0:d}", p.IdInfo.IdNumber);
+        }
+
+
         static void Main(string[] args)
         {
             // Singleton
@@ -21,6 +22,25 @@
             {
                 singleton1.ShowInfo();
             }
+
+            // Prototype
+            Person p1 = new Person();
+            p1.Age = 42;
+            p1.BirthDate = Convert.ToDateTime("1977-01-01");
+            p1.Name = "Jack Daniels";
+            p1.IdInfo = new IdInfo(666);
+
+            Person p2 = p1.ShallowCopy();
+            Person p3 = p1.DeepCopy();
+
+            Console.WriteLine("Original values of p1, p2, p3:");
+            Console.WriteLine("   p1 instance values: ");
+            DisplayValues(p1);
+            Console.WriteLine("   p2 instance values:");
+            DisplayValues(p2);
+            Console.WriteLine("   p3 instance values:");
+            DisplayValues(p3);
+
         }
     }
 }
---
+++ HW_Module2/HW_2_6_patterns/Prototype.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime;
+using System.Text;
+using System.Threading.Tasks;
+
+//Прототип — это порождающий паттерн проектирования, который позволяет копировать объекты, не вдаваясь в подробности их реализации.
+
+namespace HW_2_6_patterns
+{
+    public class Person
+    {
+        public int Age;
+        public DateTime BirthDate;
+        public string Name;
+        public IdInfo IdInfo;
+
+        public Person ShallowCopy()
+        {
+            return (Person)this.MemberwiseClone();
+        }
+
+        public Person DeepCopy()
+        {
+            Person clone = (Person)this.MemberwiseClone();
+            clone.IdInfo = new IdInfo(IdInfo.IdNumber);
+            clone.Name = String.Copy(Name);
+            return clone;
+        }
+    }
+
+    public class IdInfo
+    {
+        public int IdNumber;
+
+        public IdInfo(int idNumber)
+        {
+            this.IdNumber = idNumber;
+        }
+    }
+}
