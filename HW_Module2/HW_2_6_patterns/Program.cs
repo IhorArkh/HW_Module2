237b97ed47720a3be3ab465224d1a7d441c520f7
Ihor Arkhypenko <wasbornindecember@gmail.com>
2022-12-24 03:18:19 +0200

    Singleton added

---
+++ HW_Module2/HW_2_6_patterns/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace HW_2_6_patterns
+{
+    class Car
+    {
+
+    }
+    class CarBuilder
+    {
+        private Car car; 
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Singleton
+            Singleton singleton1 = Singleton.GetInstance();
+            Singleton singleton2 = Singleton.GetInstance();
+
+            
+            if (singleton1 == singleton2)
+            {
+                singleton1.ShowInfo();
+            }
+
+            // 
+        }
+    }
+}
